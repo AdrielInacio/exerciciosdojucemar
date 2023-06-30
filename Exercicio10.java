008ee0ab8e34dec45a2460bb22839a035d35fb10
Adriel <138242380+AdrielInacio@users.noreply.github.com>
2023-06-30 14:58:00 -0300

    Add files via upload

---
+++ Exercicio10.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Exercicio10
+{
+    public static void main(String[] args)
+    {
+        //Variaveis/inicialização
+        //Lógica
+        System.out.println("   ......");
+        System.out.println(" ..      ..");
+        System.out.println(".          .");
+        System.out.println(".          .");
+        System.out.println(" ..      ..");
+        System.out.println("   ......");
+    }
+}
